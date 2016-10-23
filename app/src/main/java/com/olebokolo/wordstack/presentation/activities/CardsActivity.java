97d15ed10e637d135fadc26f31108c65c90a2a8b
alexander <abkolodiychuk@gmail.com>
2016-10-23 23:37:18 +0100

    Created cards activity

---
+++ app/src/main/java/com/olebokolo/wordstack/presentation/activities/CardsActivity.java
@@ -0,0 +1,15 @@
+package com.olebokolo.wordstack.presentation.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.olebokolo.wordstack.R;
+
+public class CardsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cards);
+    }
+}
